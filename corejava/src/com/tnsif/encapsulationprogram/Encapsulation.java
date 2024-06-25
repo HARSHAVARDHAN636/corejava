69f01ed782c59017920dc69bd4905eb2d21e2157
HARSHAVARDHAN <harsha44029@gmail.com>
2024-06-25 18:53:29 +0530

    day 1

---
+++ corejava/src/com/tnsif/encapsulationprogram/Encapsulation.java
@@ -0,0 +1,15 @@
+
+//program to demonstrate encapsulation
+package com.tnsif.encapsulationprogram;
+
+public class Encapsulation {
+	int serialNum;
+	String name ;
+	int age;
+	void show ()//
+	{
+		System.out.print("serialnumber"+serialNum+"\t Name "+name+"\t Age:"+age );
+	}
+	
+
+}
---
+++ corejava/src/com/tnsif/encapsulationprogram/EncapuslationDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulationprogram;
+
+public class EncapuslationDemo {
+	public static void main(String[] args) {
+		Encapsulation ob=new Encapsulation();
+		ob.name="harsha";
+		ob.age=33;
+		ob.serialNum=22;
+		ob.show();
+	}
+
+}
